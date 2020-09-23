2f400c2bb88948b1a99b172e3a959f9dc1aa58df
jfcarocota <jfcarocota@gmail.com>
2020-09-22 19:22:59 -0700

    trackable object

--- Assets/Scripts/ARTracking.cs
+++ Assets/Scripts/ARTracking.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
+[RequireComponent(typeof(ARRaycastManager))]
 public class ARTracking : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    GameObject srcObject;
+
+    GameObject spawnedObj;
+    ARRaycastManager aRRaycastManager;
+    Vector2 touchPos;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake()
     {
-        
+        aRRaycastManager = GetComponent<ARRaycastManager>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if(IsTouching(out Vector2 touchPos))
+        {
+            if(aRRaycastManager.Raycast(touchPos, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose pose = hits[0].pose;
+
+                if(!spawnedObj)
+                {
+                    spawnedObj = Instantiate(srcObject, pose.position, pose.rotation);
+                }
+                else
+                {
+                    spawnedObj.transform.position = pose.position;
+                    spawnedObj.transform.rotation = pose.rotation;
+                }
+            }
+        }    
+    }
+
+
+    bool IsTouching(out Vector2 touchPos)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPos = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPos = default;
+        return false;
     }
 }
